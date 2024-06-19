341559f6c999d4e336c22fc44872ede897b654e8
arusrex <arusbr@hotmail.com>
2024-06-19 11:44:05 -0300

    att

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from home.views import index
+from home.views import index, blog, contact, about, services
 
 app_name = 'home'
 
 urlpatterns = [
     path('', index, name="index"),
+    path('blog/', blog, name="blog"),
+    path('contact', contact, name="contact"),
+    path('about/', about, name="about"),
+    path('services/', services, name="services"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/pages/index.html')
+
+def blog(request):
+    return render(request, 'home/pages/blog.html')
+
+def contact(request):
+    return render(request, 'home/pages/contact.html')
+
+def about(request):
+    return render(request, 'home/pages/about.html')
+
+def services(request):
+    return render(request, 'home/pages/services.html')
